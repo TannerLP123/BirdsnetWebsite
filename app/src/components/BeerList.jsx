586b99512e0f532c3c0645b2a4f75fa7f5ff928a
TannerLP123 <tanner.poston@g.fmarion.edu>
2023-10-10 11:53:24 -0400

    Refactored the local beers to be mapped over

--- app/src/components/BeerList.jsx
+++ app/src/components/BeerList.jsx
@@ -1,36 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "../css/Beerlist.css";
 
-const BeerList = () => {
-  return (
-    <>
-      <h1 class = "beerList">Pick Your Beer Here:</h1>
-      <img class = "beerListImg" src = "" alt = "A delicous looking beer" title = "a picture of a beer"/>
-      <div class = "beerContainer">
-        <ul class = "domestics">Domestics:
-          <li>Michelob Ultra</li>
-          <li>Budweiser</li>
-          <li>Bud Light</li>
-          <li>Miller Light</li>
-          <li>Coors Banquet</li>
-          <li>Coors Light</li>
-          <li>Natural Light</li>
-          <li>Yeingling</li>
-          <li>Amstel</li>
-          <li>Dos Equis</li>
-          <li>Pabst Blue Ribbon</li>
-        </ul>
+let localBreweries = [
+  {
+    id: 1,
+    nameOfBrewery: "Edmond's Oast",
+    city: "Charleston",
+    state: "SC"
+  },
+  {
+    id: 2,
+    nameOfBrewery: "Westbrook",
+    city: "Mount Plesant",
+    state: "SC"
+  },
+  {
+    id: 3,
+    nameOfBrewery: "River Rat",
+    city: "Columbia",
+    state: "SC"
+  },
+  {
+    id: 4,
+    nameOfBrewery: "Seminar",
+    city: "Florence",
+    state: "SC"
+  },
+  {
+    id: 5,
+    nameOfBrewery: "Columbia Craft",
+    city: "Columbia",
+    state: "SC"
+  }
+]
 
-        <ul class = "locals">Local Brewery's:
-          <li>Edmond's Oast Brewing Co.....Charleston, SC</li>
-          <li>Westbrook Brewing Co.....Mount Pleasant, SC</li>
-          <li>River Rat Brewing Co.....Columbia, SC</li>
-          <li>Seminar Brewing Co.....Florence, SC</li>
-          <li>Columbia Craft Brewing Co.....Columbia, SC</li>
-        </ul>
+class BeerList extends Component {
+  render() {
+
+    let localBrewMap = localBreweries.map((locals) => {
+      return(
+        <div key = {locals.id}>
+          <h2>{locals.nameOfBrewery}</h2>
+          <h4>{locals.city}, {locals.state}</h4>
+        </div>
+      )
+    })
+
+    return (
+      <div className="beer-list">
+      <h1 className = "beerList">Pick Your Beer Here:</h1>
+      <img className = "beerListImg" src = "" alt = "A delicous looking beer" title = "a picture of a beer"/>
+        <div className = "beerContainer">
+          <ul className = "domestics">Domestics:
+            <li>Michelob Ultra</li>
+            <li>Budweiser</li>
+            <li>Bud Light</li>
+            <li>Miller Light</li>
+            <li>Coors Banquet</li>
+            <li>Coors Light</li>
+            <li>Natural Light</li>
+            <li>Yeingling</li>
+            <li>Amstel</li>
+            <li>Dos Equis</li>
+            <li>Pabst Blue Ribbon</li>
+          </ul>
+
+          <div className = "LocalsList">
+            {localBrewMap}
+          </div>
+        </div>
       </div>
-    </>
-  )
+    );
+  }
 }
 
-export default BeerList
+export default BeerList;
